6298dbff472a05d5fe83b1c5182d9c64837eb36e
Anton <patsev.anton@gmail.com>
2026-01-14 11:43:49 +0600

    add yandex_dns_recordset

--- dns.tf
+++ dns.tf
@@ -17,3 +17,12 @@ resource "yandex_dns_zone" "apatsev-org-ru" {
 
   private_networks = [yandex_vpc_network.k8s-network.id]
 }
+
+# Создание DNS-записи типа A, указывающей на внешний IP
+resource "yandex_dns_recordset" "redis1" {
+  zone_id = yandex_dns_zone.apatsev-org-ru.id                          # ID зоны, к которой принадлежит запись
+  name    = "redis1.apatsev.org.ru."                                  # Полное имя записи (поддомен)
+  type    = "A"                                                        # Тип записи — A (IPv4-адрес)
+  ttl     = 200                                                        # Время жизни записи в секундах
+  data    = [yandex_vpc_address.addr.external_ipv4_address[0].address] # Значение — внешний IP-адрес, полученный ранее
+}
